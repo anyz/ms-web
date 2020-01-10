b1a161b46840b3410765119457768d5dc2dea4f0
anyz.khan@gmail.com <anyz.khan@gmail.com>
2020-01-10 17:47:43 -0600

    added contact page

--- src/main/java/com/ms/msweb/NavigationController.java
+++ src/main/java/com/ms/msweb/NavigationController.java
@@ -14,17 +14,35 @@ import javax.validation.ConstraintViolationException;
 @Controller
 public class NavigationController {
 
+    static final String ACTIVE = "active";
     static final String HOME = "index";
     static final String PROGRAMS = "programs";
+    static final String CONTACTUS = "contactus";
+
 
     @RequestMapping(value="/", method = RequestMethod.GET)
+    public String defaultHome(Model model){
+        model.addAttribute(HOME, ACTIVE);
+        return HOME;
+    }
+
+
+    @RequestMapping(value="/home", method = RequestMethod.GET)
     public String home(Model model){
+        model.addAttribute(HOME, ACTIVE);
         return HOME;
     }
 
     @RequestMapping(value="/programs", method = RequestMethod.GET)
     public String programs(Model model){
+        model.addAttribute(PROGRAMS, ACTIVE);
         return PROGRAMS;
+    }
+
+    @RequestMapping(value="/contactus", method = RequestMethod.GET)
+    public String contactus(Model model){
+        model.addAttribute(CONTACTUS, ACTIVE);
+        return CONTACTUS;
     }
 
 
